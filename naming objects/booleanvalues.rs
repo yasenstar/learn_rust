8a8d0f8b136b0eac61d21b0f14b5c2453148bdee
yasenstar <xiaoqizhao@outlook.com>
2020-02-22 08:55:20 -0500

    update with compiling

---
+++ naming objects/booleanvalues.rs
@@ -0,0 +1,7 @@
+// To present truth values, the keywords `true` and `false` are used.
+// Such keywords are expressions having a non-numeric type, named "Boolean".
+fn main(){
+    let truth = true;
+    let falsity = false;
+    print!("{} {}", truth, falsity);
+}
