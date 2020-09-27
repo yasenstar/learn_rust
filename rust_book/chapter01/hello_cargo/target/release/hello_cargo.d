94f6dca116cb487b9a20278ddb23c3d3535d5bf1
yasenstar <xiaoqizhao@outlook.com>
2020-09-27 12:00:40 -0400

    cargo build and release

---
+++ rust_book/chapter01/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/yasen/Documents/GitHub/learn_rust/rust_book/chapter01/hello_cargo/target/release/hello_cargo: /home/yasen/Documents/GitHub/learn_rust/rust_book/chapter01/hello_cargo/src/main.rs
