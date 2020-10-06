fef64b7c33762df8f73e2e14898a92f548653664
Yasen Zhao <xiaoqizhao@outlook.com>
2020-10-06 18:09:15 -0400

    update

--- beginning_rust/ch01_printing-on-term/smallest_rust_prog.rs
+++ beginning_rust/ch01_printing-on-term/smallest_rust_prog.rs
@@ -2,4 +2,6 @@ fn main() {
     println!("Hello World");
     println!("{}, {}!", "Hello", "World");
     println!("First Line\nSecond Line\nThird Line");
+    println!("My number is: {}", 140);
+    println!("{}: {}", "My number", 140);
 }
