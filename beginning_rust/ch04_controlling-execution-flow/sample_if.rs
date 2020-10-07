5d752ad8323082c69ac36c334e70f27f2a05271a
Yasen Zhao <xiaoqizhao@outlook.com>
2020-10-06 20:43:49 -0400

    update

---
+++ beginning_rust/ch04_controlling-execution-flow/sample_if.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let n = -4;
+    if n > 0 {
+        println!("positive");
+    }
+    else {
+        println!("not positive");
+    }
+}
---
+++ beginning_rust/ch04_controlling-execution-flow/sample_while.rs
@@ -0,0 +1,25 @@
+fn main() {
+/*     let mut i = 1;
+    while i < 100 {
+        print!("{} ", i * i);
+        i += 1;
+    } */
+
+/*     let mut i = 0;
+    while i < 50 {
+        i += 1;
+        if i % 3 != 0 {
+            if i * i <= 400 {
+                print!("{} ", i * i);
+            }
+        }
+    } */
+
+    let mut i = 0;
+    while i < 100 {
+        i += 1;
+        if i % 3 == 0 {continue;}
+        if i * i > 4000 {break;}
+        print!("{} ", i * i);
+    }
+}
