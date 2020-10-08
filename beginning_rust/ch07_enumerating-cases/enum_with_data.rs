6076869505869717e2561aa41ebcf5dfd5d38b6d
Yasen Zhao <xiaoqizhao@outlook.com>
2020-10-07 20:12:47 -0400

    emum with data

--- beginning_rust/ch07_enumerating-cases/enum_with_data.rs
+++ beginning_rust/ch07_enumerating-cases/enum_with_data.rs
@@ -1,6 +1,6 @@
 // Rust enumerations can work with data
 
-fn main() {
+/* fn main() {
     enum Result {
         Success(f64),
         Failure(u16, char),
@@ -12,5 +12,30 @@ fn main() {
         Result::Success(value) => print!("Result: {}", value),
         Result::Failure(error_code, module) => print!("Error n. {} in module {}", error_code, module),
         Result::Uncertainty => {},
+    }
+} */
+
+// "match" expressions
+
+/* fn main() {
+    enum CardinalPoint { North, South, West, East };
+    let direction = CardinalPoint::East;
+    print!("{}", match direction {
+        CardinalPoint::North => 'N',
+        CardinalPoint::South => 'S',
+        _ => '*',
+    });
+} */
+
+// Use of Guards in match Constructs
+
+fn main() {
+    for n in -5..7 {
+        println!("{} is {}.", n, match n {
+            0 => "zero",
+            1 => "one",
+            _ if n < 0 => "negative",
+            _ => "plural",
+        })
     }
 }
