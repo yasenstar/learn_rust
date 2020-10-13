d7c2531f67dbcff739d352a0b67eb729d4b70f06
Yasen Zhao <xiaoqizhao@outlook.com>
2020-10-13 18:44:47 -0400

    update

--- beginning_rust/ch13_defining-closures/capture_env.rs
+++ beginning_rust/ch13_defining-closures/capture_env.rs
@@ -1,7 +1,15 @@
-fn main() {
+/* fn main() {
     let two  = 2.;
     fn print_double(x: f64) {
         print!("{}", x * two);
     }
     print_double(17.2);
+} */
+
+fn main() {
+    const TWO: f64 = 2.;
+    fn print_double(x: f64) {
+        print!("{}", x * TWO);
+    }
+    print_double(17999.2332);
 }
