f36846ad3d0e4bb7c2059afe04b676a5cec8162c
Yasen Zhao <xiaoqizhao@outlook.com>
2020-10-13 20:30:40 -0400

    update

--- the_rust_lang/ch02_pramming-guessing-game/guessing_game/src/main.rs
+++ the_rust_lang/ch02_pramming-guessing-game/guessing_game/src/main.rs
@@ -16,5 +16,16 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line!");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+        
     println!("Your guess is: {}", guess);
+
+    // compare the guess to the secret number
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
