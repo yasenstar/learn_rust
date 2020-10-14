05b5fe792ee810aa067520b89c0b7c7980e4590a
Yasen Zhao <xiaoqizhao@outlook.com>
2020-10-13 20:00:54 -0400

    guessing game

---
+++ the_rust_lang/ch02_pramming-guessing-game/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/yasen/Documents/GitHub/learn_rust/the_rust_lang/ch02_pramming-guessing-game/guessing_game/target/debug/guessing_game: /Users/yasen/Documents/GitHub/learn_rust/the_rust_lang/ch02_pramming-guessing-game/guessing_game/src/main.rs
---
+++ the_rust_lang/ch02_pramming-guessing-game/guessing_game/target/rls/debug/deps/guessing_game-d519c30a04c83fb4.d
@@ -0,0 +1,5 @@
+/Users/yasen/Documents/GitHub/learn_rust/the_rust_lang/ch02_pramming-guessing-game/guessing_game/target/rls/debug/deps/guessing_game-d519c30a04c83fb4.rmeta: src/main.rs
+
+/Users/yasen/Documents/GitHub/learn_rust/the_rust_lang/ch02_pramming-guessing-game/guessing_game/target/rls/debug/deps/guessing_game-d519c30a04c83fb4.d: src/main.rs
+
+src/main.rs:
