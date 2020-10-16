c6ea80ddf3b7d2145a001e8e23841db727013560
Yasen Zhao <xiaoqizhao@outlook.com>
2020-10-16 17:20:30 -0400

    create guessing_game

---
+++ the_rust_lang/ch02_pramming-guessing-game/guessing_game/target/rls/debug/deps/guessing_game-e35ed72eb579571c.d
@@ -0,0 +1,5 @@
+/Users/yasen/Documents/GitHub/learn_rust/the_rust_lang/ch02_pramming-guessing-game/guessing_game/target/rls/debug/deps/guessing_game-e35ed72eb579571c.rmeta: src/main.rs
+
+/Users/yasen/Documents/GitHub/learn_rust/the_rust_lang/ch02_pramming-guessing-game/guessing_game/target/rls/debug/deps/guessing_game-e35ed72eb579571c.d: src/main.rs
+
+src/main.rs:
